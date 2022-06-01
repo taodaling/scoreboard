ff699ad05c693b25940912e75d58ade9505ade18
root <997962520@qq.com>
2022-06-01 20:10:58 +0800

    add roles

--- src/main/java/org/happyhour/scoreboard/enums/RoleEnum.java
+++ src/main/java/org/happyhour/scoreboard/enums/RoleEnum.java
@@ -2,6 +2,7 @@ package org.happyhour.scoreboard.enums;
 
 public enum RoleEnum {
 
+    // --阿瓦隆
     LOYAL_SERVANT(0x0000, "忠臣"),
     MERLIN(0x0001, "梅林"),
     PERCIVAL(0x0002, "派西维尔"),
@@ -10,7 +11,30 @@ public enum RoleEnum {
     ASSASSIN(0x0101, "刺客"),
     MORGANA(0x0102, "莫甘娜"),
     MORDRED(0x0103, "莫德雷德"),
-    OBERON(0x0104, "奥博伦");
+    OBERON(0x0104, "奥博伦"),
+    // --阿瓦隆
+
+    // --狼人杀
+    VILLAGER(0x1000, "村民"),
+
+    POLICE_CHIEF(0x1100, "警长"),
+    PREDICTOR(0x1101, "预言家"),
+    WITCH(0x1102, "女巫"),
+    HUNTER(0x1103, "猎人"),
+    FOOL(0x1104, "白痴"),
+    GUARD(0x1105, "守卫"),
+    KNIGHT(0x1106, "骑士"),
+    GRAVE_GUARD(0x1107, "守墓人"),
+
+
+    WEREWOLF(0x1200, "狼人"),
+    WOLF_KING(0x1201, "狼王"),
+    WHITE_WOLF_KING(0x1202, "白狼王"),
+    WOLF_BEAUTY(0x1203, "狼美人"),
+    SNOW_WOLF(0x1204, "雪狼"),
+    GARGOYLE(0x1205, "石像鬼"),
+    GHOST_RIDER(0x1206, "恶灵骑士");
+    // --狼人杀
 
     public int id;
     public String name;
